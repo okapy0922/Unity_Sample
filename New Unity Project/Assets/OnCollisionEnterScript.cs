058ae6ae4861d30205ce38a0831413e08ad86f07
OkadaTakumi <okada@itpm-gk.com>
2019-10-23 00:51:19 +0900

    TEST

---
+++ New Unity Project/Assets/OnCollisionEnterScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnCollisionEnterScript : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+}
