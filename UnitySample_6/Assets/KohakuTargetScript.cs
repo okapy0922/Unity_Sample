f255846a3e3ea6806a40aa3c969b73c8c26eef5f
OkadaTakumi <okada@itpm-gk.com>
2019-10-22 23:14:43 +0900

    first_commit

---
+++ UnitySample_6/Assets/KohakuTargetScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class KohakuTargetScript : MonoBehaviour
+{
+    public GameObject target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        // ナビメッシュエージェントが設定されているネコがtargetの後をつけていく
+        agent.destination = target.transform.position;
+    }
+}
