a2658098b6ca109d9a0cf5d932e1afaca8ee9b73
OkadaTakumi <okada@itpm-gk.com>
2019-11-09 15:56:41 +0900

    20191109_commit

---
+++ New Unity Project (1)/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
+    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
+    [RequireComponent(typeof (ThirdPersonCharacter))]
+    public class AICharacterControl : MonoBehaviour
+    {
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // パス検索に必要なnavmeshエージェント
+        public ThirdPersonCharacter character { get; private set; } // 制御しているキャラクター
+        public Transform target;                                    // 目指すターゲット
+
+
+        private void Start()
+        {
+            // 必要なオブジェクトのコンポーネントを取得する（コンポーネントを必要とするためnullにしない）
+            agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+            character = GetComponent<ThirdPersonCharacter>();
+
+	        agent.updateRotation = false;
+	        agent.updatePosition = true;
+        }
+
+
+        private void Update()
+        {
+            if (target != null)
+                agent.SetDestination(target.position);
+
+            if (agent.remainingDistance > agent.stoppingDistance)
+                character.Move(agent.desiredVelocity, false, false);
+            else
+                character.Move(Vector3.zero, false, false);
+        }
+
+
+        public void SetTarget(Transform target)
+        {
+            this.target = target;
+        }
+    }
+}
