1eadd331476f1f4e6ee56101c3bad170f837d077
OkadaTakumi <okada@itpm-gk.com>
2019-12-11 01:02:07 +0900

    20191211_commit

---
+++ UnityGOD_2/Assets/DropTofu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class DropTofu : MonoBehaviour
+{
+    [SerializeField] GameObject tofu;
+    // 補足:[SerializeField] シリアライゼーション、Unity上で編集できるようにする
+    // https://qiita.com/makopo/items/8ef280b00f1cc18aec91
+
+    int TofuCount = 0;
+
+    void Start()
+    {
+        InvokeRepeating("DropOne", 2f, 1f);
+        // 2秒経ったらDropOneメソッドを呼び出す、以降1秒ごとに呼び出す
+    }
+
+    void DropOne()
+    // DropOneが呼ばれたら以下を実行する
+    {
+        TofuCount++;
+        // TofuCountの値を一つ追加
+        Instantiate(tofu , new Vector3(2.5f , 5.0f , 0.0f) , Quaternion.identity);
+        // 変数tofuに設定されているGameObject(tofu)をY軸5Mの高さに生成して囲いの中に入るようにする
+        // https://qiita.com/Teach/items/c28b4fe5ca8dc4c83e26
+        if (TofuCount == 100)
+        {
+            CancelInvoke();
+            // tofuが100個生成されたら繰り返し呼び出しを中止する
+        }
+    }
+}
