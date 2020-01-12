4358a33c5bb4087940f6cc7b72695f9217f33e73
OkadaTakumi <okada@itpm-gk.com>
2020-01-12 14:23:29 +0900

    20200112_commit

---
+++ UnityGod_4/Assets/Signal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Signal : MonoBehaviour
+{
+    public GameObject RedCube;
+    public GameObject BlueCube;
+
+    public Material MRed;
+    public Material MRedDark;
+    public Material MBlue;
+    public Material MBlueDark;
+
+    public int ChangeCount = 0;
+
+    // 信号機の最初の状態から3秒経ったら5秒間隔でChangesignalを呼び出す
+    void Start()
+    {
+        RedCube.GetComponent<Renderer>().material = MRed;
+        BlueCube.GetComponent<Renderer>().material = MBlueDark;
+        // InvokeRepeatingでChangeSignalを繰り返す
+        InvokeRepeating("ChangeSignal", 3f, 5f);
+    }
+
+    void ChangeSignal()
+    {
+        ChangeCount++;
+        if(ChangeCount % 2 == 0)
+        {
+            RedCube.GetComponent<Renderer>().material = MRedDark;
+            BlueCube.GetComponent<Renderer>().material = MBlue;
+
+        }else{
+            RedCube.GetComponent<Renderer>().material = MRed;
+            BlueCube.GetComponent<Renderer>().material = MBlueDark;
+        }
+
+    }
+}
