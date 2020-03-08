f82a67e6417756e31558976d3ea42ee19ac81366
OkadaTakumi <okada@itpm-gk.com>
2020-03-08 20:24:48 +0900

    20200308_commit

--- UnityGod_4/Assets/BulletBehaviour.cs
+++ UnityGod_4/Assets/BulletBehaviour.cs
@@ -6,9 +6,18 @@ public class BulletBehaviour : MonoBehaviour
     // 当たった時に呼ばれる関数 OnCollisionEnter
     void OnCollisionEnter(Collision collision)
     {
+        // [玉が着弾したらAdd_Damageを送る、Add_Damageメソッドを持っているオブジェクトに着弾すると処理が動く]
+
+        // （Unity側で）タグにCarがついてるGameObjectに着弾したらAdd_Damageを送る
+        if (collision.gameObject.tag == "Car")
+            // Add_Damageを送る、メソッドを持っているスクリプトが反応する
+            // SendMessageには引数が１つしかつけられない、エラーを出力しないため注意が必要？
+            collision.gameObject.SendMessage("Add_Damage");
+
         // 玉が何かにぶつかったら爆発する処理
-        // Instantiateで生成する、ExplosionPrefabを元に玉が爆発
+        // Instantiateで爆破エフェクト表示、ExplosionPrefabを元に玉が爆発
         Instantiate(ExplosionPrefab, transform.position, transform.rotation);
+
         // 玉がなくなる処理
         Destroy(gameObject);
 
