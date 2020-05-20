c953fb31c0e84b3857965cc0f726b69dd7b74a20
OkadaTakumi <okada@itpm-gk.com>
2020-05-20 17:41:44 +0900

    20200416_commit

--- UnityGod4-2/Assets/InteractiveObject.cs
+++ UnityGod4-2/Assets/InteractiveObject.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractiveObject : MonoBehaviour
-{
+public class InteractiveObject : MonoBehaviour{
     // Vector3型 引き出しが開いた時(openPositon)と閉じたとき(closePosition)の座標位置をインスペクタ表示、数値入力で設定
     [SerializeField] private Vector3 openPositon, closedPosition;
 
@@ -20,6 +19,9 @@ public class InteractiveObject : MonoBehaviour
     // (これがないと引き出しを開けたとき空間に飛び出たままになった)
     private Hashtable iTweenArgs;
 
+    // audioSource作成(オーディオクリップを再生するための変数)
+    private AudioSource audioSource;
+
     void Start(){
         /* P.234 ハッシュテーブル作成_渡す引数（arguments）は2個ペア
         Property NameとTypeをカンマ区切りで追加する*/
@@ -31,23 +33,29 @@ public class InteractiveObject : MonoBehaviour
         // 「islocal」ワールド空間でアニメ化するか、親(引き出し)を基準にしてアニメ化するか。
         // （デフォルト値はfalseでtrueにすることで親オブジェクトを基準としている）
         iTweenArgs.Add("islocal", true);
+
+        // オーディオソースのコンポーネント取得(GetComponentでAudioSourceを取得)
+        audioSource = GetComponent<AudioSource>();
     }
 
-    void Update(){
-        // Eキーが押下されたら
-        if (Input.GetKeyDown(KeyCode.E)){
-            // （isOpenの値が開いているか閉じているかの状態を保持している）
-            if (isOpen){
-                // 引き出しが開いていたら閉じる
-                iTweenArgs["position"] = closedPosition;
-            }else{
-                // 引き出しが閉じていたら開く
-                iTweenArgs["position"] = openPositon;
-            }
-            //  (真偽を交互に置き換えする、ここにこれがないとEボタン連打したときに引き出しの開け閉めがうまく動作しなかった)
-            isOpen = !isOpen;
-            // インスペクタで設定した座標位置にアニメーション移動（引き出し開閉）
-            iTween.MoveTo(gameObject, iTweenArgs);
+    // 他のスクリプトからアクセスが可能となるようにpublic宣言
+    public void PerformAction(){
+        // 引き出しが開閉されたら開閉音が再生される
+        if (audioSource) {
+            // 与えたオーディオクリップを取得して自動的に再生
+            audioSource.Play();
         }
+        // （isOpenの値が開いているか閉じているかの状態を保持している）
+        if (isOpen){
+        // 引き出しが開いていたら閉じる
+       iTweenArgs["position"] = closedPosition;
+       }else{
+       // 引き出しが閉じていたら開く
+       iTweenArgs["position"] = openPositon;
+       }
+       //  (真偽を交互に置き換えする、ここにこれがないとEボタン連打したときに引き出しの開け閉めがうまく動作しなかった)
+       isOpen = !isOpen;
+       // インスペクタで設定した座標位置にアニメーション移動（引き出し開閉）
+       iTween.MoveTo(gameObject, iTweenArgs);
     }
 }
