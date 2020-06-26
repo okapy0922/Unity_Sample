c5b06bc6e764f62c2837201556750135b372c990
OkadaTakumi <okada@itpm-gk.com>
2020-06-26 13:56:31 +0900

    20200626_commit

--- UnityGod4-2/Assets/InteractiveObject.cs
+++ UnityGod4-2/Assets/InteractiveObject.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractiveObject : MonoBehaviour{
-    // Vector3型 引き出しが開いた時(openPositon)と閉じたとき(closePosition)の座標位置をインスペクタ表示、数値入力で設定
-    [SerializeField] private Vector3 openPositon, closedPosition;
+    // Vector3型 引き出しが開いた時(openPosition)と閉じたとき(closePosition)の座標位置をインスペクタ表示、数値入力で設定
+    [SerializeField] private Vector3 openPosition, closedPosition;
 
     // Float型 引き出しの開閉スピードをインスペクタ表示、数値入力で設定
     [SerializeField] private float animationTime;
@@ -13,6 +13,12 @@ public class InteractiveObject : MonoBehaviour{
      (これがないとEキーで開いたとき、引き出しが閉まらなくなる)
      ブール値はデフォルトの状態でfalseにする*/
     [SerializeField] private bool isOpen = false;
+
+    // 移動タイプをインスペクタ内で表示、オブジェクトごとにタイプを選択する
+    [SerializeField] private MovementType movementType;
+
+    //  ゲームオブジェクトをスライド移動させるか回転移動させるか移動タイプの用意
+    private enum MovementType{Slide, Rotate};
 
     // God_P.234 ハッシュテーブル：キーと値の組み合わせの情報で、
     // キーにより値を出し入れすることができる連想配列と呼ばれる配列の一種のこと
@@ -27,7 +33,9 @@ public class InteractiveObject : MonoBehaviour{
         Property NameとTypeをカンマ区切りで追加する*/
         iTweenArgs = iTween.Hash();
         // GameObjectがアニメーション化する空間内のポイント。
-        iTweenArgs.Add("position", openPositon);
+        iTweenArgs.Add("position", openPosition);
+        iTweenArgs.Add("rotation", openPosition);
+
         // アニメーションが完了するまでの秒数
         iTweenArgs.Add("time", animationTime);
         // 「islocal」ワールド空間でアニメ化するか、親(引き出し)を基準にしてアニメ化するか。
@@ -45,17 +53,33 @@ public class InteractiveObject : MonoBehaviour{
             // 与えたオーディオクリップを取得して自動的に再生
             audioSource.Play();
         }
+
+        if (Input.GetKeyDown(KeyCode.E)) {
         // （isOpenの値が開いているか閉じているかの状態を保持している）
         if (isOpen){
-        // 引き出しが開いていたら閉じる
+        // 開いていたら閉じる
        iTweenArgs["position"] = closedPosition;
+       iTweenArgs["rotation"] = closedPosition;
        }else{
-       // 引き出しが閉じていたら開く
-       iTweenArgs["position"] = openPositon;
+       // 閉じていたら開く
+       iTweenArgs["position"] = openPosition;
+       iTweenArgs["rotation"] = openPosition;
+
        }
        //  (真偽を交互に置き換えする、ここにこれがないとEボタン連打したときに引き出しの開け閉めがうまく動作しなかった)
        isOpen = !isOpen;
-       // インスペクタで設定した座標位置にアニメーション移動（引き出し開閉）
-       iTween.MoveTo(gameObject, iTweenArgs);
+
+            // スイッチ文を使用してMovementTypeを判定する（スライドとローテート以外の動きを追加するときに簡潔になる）
+            switch (movementType) {
+                // MovementTypeがスライド移動であればSlide開閉を実行する
+                case MovementType.Slide:
+                    iTween.MoveTo(gameObject, iTweenArgs);
+                    break;
+                // MovementTypeがローテート移動であればRotate開閉を実行する
+                case MovementType.Rotate:
+                    iTween.RotateTo(gameObject, iTweenArgs);
+                    break;
+            }
+        }
     }
 }
